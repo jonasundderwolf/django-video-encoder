ef891e36a75e4ab4c8d078ca364c5ce6b1ff088a
Jonas <jvp@jonasundderwolf.de>
2014-08-18 16:01:58 +0200

    Re-use full name of video field for format file

--- django_zencoder/models.py
+++ django_zencoder/models.py
@@ -39,9 +39,8 @@ class Format(models.Model):
 
     format = models.CharField(max_length=255, choices=[
         (f['label'], f['label']) for f in settings.ZENCODER_FORMATS])
-    file = models.FileField(upload_to=lambda i, f: 'formats/%s/%s%s' % (
-        i.format, splitext(basename(getattr(i.video, i.field_name).path))[0],
-        splitext(f)[1]), max_length=2048)
+    file = models.FileField(upload_to=lambda i, f: 'formats/%s/%s' % (
+        i.format, getattr(i.video, i.field_name).name), max_length=2048)
     width = models.PositiveIntegerField('Width', null=True)
     height = models.PositiveIntegerField('Height', null=True)
     duration = models.PositiveIntegerField('Duration (ms)', null=True)
