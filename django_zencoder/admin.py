707b98adad079ab6374284f77c2e88f3c08ba9f8
Jonas <jvp@jonasundderwolf.de>
2015-02-10 14:05:27 +0100

    Add admin inline for formats

---
+++ django_zencoder/admin.py
@@ -0,0 +1,10 @@
+from django.contrib.contenttypes import admin
+from .models import Format
+
+
+class FormatInline(admin.GenericTabularInline):
+    model = Format
+    fields = ('format', 'file', 'width', 'height', 'duration')
+    readonly_fields = fields
+    extra = 0
+    max_num = 0
