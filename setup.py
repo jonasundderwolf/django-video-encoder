ae368c14cd0e1543e35b6ae85ac8b212bb0e5783
Jonas <jvp@jonasundderwolf.de>
2015-02-13 15:07:11 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name="django_zencoder",
     author="Jonas und der Wolf",
     author_email="info@jonasunderwolf.de",
-    version="0.2",
+    version="0.3",
     packages=find_packages(),
     package_data=finddata.find_package_data(),
     include_package_data=True,
