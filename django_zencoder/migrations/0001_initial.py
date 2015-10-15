f39c33a493b8458a4dc2b15ebcac1af787da08d3
Jonas <jvp@jonasundderwolf.de>
2015-10-15 15:38:21 +0200

    Add Django 1.8 migration

--- django_zencoder/migrations/0001_initial.py
+++ django_zencoder/migrations/0001_initial.py
@@ -1,55 +1,30 @@
 # -*- coding: utf-8 -*-
-from south.utils import datetime_utils as datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django_zencoder.models
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'Format'
-        db.create_table(u'django_zencoder_format', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('object_id', self.gf('django.db.models.fields.PositiveIntegerField')()),
-            ('content_type', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['contenttypes.ContentType'])),
-            ('field_name', self.gf('django.db.models.fields.CharField')(max_length=255)),
-            ('format', self.gf('django.db.models.fields.CharField')(max_length=255)),
-            ('file', self.gf('django.db.models.fields.files.FileField')(max_length=2048)),
-            ('width', self.gf('django.db.models.fields.PositiveIntegerField')(null=True)),
-            ('height', self.gf('django.db.models.fields.PositiveIntegerField')(null=True)),
-            ('duration', self.gf('django.db.models.fields.PositiveIntegerField')(null=True)),
-            ('extra_info', self.gf('django.db.models.fields.TextField')(blank=True)),
-        ))
-        db.send_create_signal(u'django_zencoder', ['Format'])
+    dependencies = [
+        ('contenttypes', '0002_remove_content_type_name'),
+    ]
 
-
-    def backwards(self, orm):
-        # Deleting model 'Format'
-        db.delete_table(u'django_zencoder_format')
-
-
-    models = {
-        u'contenttypes.contenttype': {
-            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
-            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
-        },
-        u'django_zencoder.format': {
-            'Meta': {'object_name': 'Format'},
-            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
-            'duration': ('django.db.models.fields.PositiveIntegerField', [], {'null': 'True'}),
-            'extra_info': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
-            'field_name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
-            'file': ('django.db.models.fields.files.FileField', [], {'max_length': '2048'}),
-            'format': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
-            'height': ('django.db.models.fields.PositiveIntegerField', [], {'null': 'True'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'object_id': ('django.db.models.fields.PositiveIntegerField', [], {}),
-            'width': ('django.db.models.fields.PositiveIntegerField', [], {'null': 'True'})
-        }
-    }
-
-    complete_apps = ['django_zencoder']
+    operations = [
+        migrations.CreateModel(
+            name='Format',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('object_id', models.PositiveIntegerField()),
+                ('field_name', models.CharField(max_length=255)),
+                ('format', models.CharField(max_length=255, choices=[(b'mp4-high', b'mp4-high'), (b'mp4-low', b'mp4-low'), (b'webm-high', b'webm-high'), (b'webm-low', b'webm-low')])),
+                ('file', models.FileField(max_length=2048, upload_to=django_zencoder.models.format_upload_to)),
+                ('width', models.PositiveIntegerField(null=True, verbose_name=b'Width')),
+                ('height', models.PositiveIntegerField(null=True, verbose_name=b'Height')),
+                ('duration', models.PositiveIntegerField(null=True, verbose_name=b'Duration (ms)')),
+                ('extra_info', models.TextField(verbose_name=b'Zencoder information (JSON)', blank=True)),
+                ('content_type', models.ForeignKey(to='contenttypes.ContentType')),
+            ],
+        ),
+    ]
