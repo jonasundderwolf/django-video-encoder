424fae6cabbb84c36d072de52ee4d589e7d3be5c
Jonas <jvp@jonasundderwolf.de>
2016-05-10 10:02:35 +0200

    Add trove classifiers

--- setup.py
+++ setup.py
@@ -9,4 +9,13 @@ setup(
     packages=find_packages(),
     package_data=finddata.find_package_data(),
     include_package_data=True,
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
 )
