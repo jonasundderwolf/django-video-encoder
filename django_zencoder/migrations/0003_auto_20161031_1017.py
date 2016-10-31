fb733d3ff9f6527d81d16e9719b514ebfb3948ef
Daniel Rios <misterrios@gmail.com>
2016-10-31 10:38:01 +0100

    add migration for redefined video choices

---
+++ django_zencoder/migrations/0003_auto_20161031_1017.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('django_zencoder', '0002_auto_20161027_1629'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='format',
+            name='format',
+            field=models.CharField(max_length=255, choices=[('H.264 (HD)', 'H.264 (HD)'), ('H.264', 'H.264'), ('VP8 (HD)', 'VP8 (HD)'), ('VP8', 'VP8')]),
+        ),
+    ]
