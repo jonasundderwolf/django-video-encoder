552ae35aa144eb07834dc2a281b0f998652a1f48
Norbert Stüken <norbert.stueken@jonasundderwolf.de>
2017-10-20 19:23:04 +0200

    Replace string view argument to url() as removed in 1.10

--- django_zencoder/urls.py
+++ django_zencoder/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 
+from .views import notification
+
 urlpatterns = [
     'django_zencoder.views',
-    url(r'notify/', 'notification', name='zencoder_notification'),
+    url(r'notify/', notification, name='zencoder_notification'),
 ]
