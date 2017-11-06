21bb1726e4a62c69c8f7fd2a55fd698227b0df6f
Norbert Stüken <norbert.stueken@jonasundderwolf.de>
2017-11-06 12:18:17 +0100

    Update urlpatterns to list as patterns() is removed in 1.10

--- django_zencoder/urls.py
+++ django_zencoder/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from .views import notification
 
-urlpatterns = patterns(
+urlpatterns = [
     'django_zencoder.views',
     url(r'notify/', notification, name='zencoder_notification'),
-)
+]
