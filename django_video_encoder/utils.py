e4e3e66b0bfdf4916c93e8eedd642bdaa136dbe2
Matthieu Rigal <matthieu.rigal@jonasundderwolf.de>
2020-10-28 17:41:57 +0100

    fix(utils): Improve resolution formatting

--- django_video_encoder/utils.py
+++ django_video_encoder/utils.py
@@ -1,15 +1,13 @@
 FULL_RESOLUTION = "full resolution"
 
 
-def format_label_to_codec_width_height(format_label):
-    video_codec, resolution = format_label[:-1].rsplit(" (", 1)
-    if resolution == FULL_RESOLUTION:
-        width = height = None
-    else:
-        width, height = resolution.split("x")
-    return video_codec, width, height
-
-
 def codec_width_height_to_format_label(video_codec, width, height):
-    resolution = f"{width}x{height}" if width or height else FULL_RESOLUTION
+    if width and height:
+        resolution = f"{width}x{height}"
+    elif not width and not height:
+        resolution = FULL_RESOLUTION
+    elif width:
+        resolution = f"{width} width"
+    else:
+        resolution = f"{height} heigth"
     return f"{video_codec} ({resolution})"
